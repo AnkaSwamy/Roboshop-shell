943aaba50d2292ddc2f1efd7d804fab9da67d0d3
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-01 14:41:20 +0530

    start nginx server with readble steps

--- 1.frontend.sh
+++ 1.frontend.sh
@@ -1,13 +1,13 @@
-echo -e "\e[31m Installing Nginx server \ e[0m"
+echo -e "\e[36m Installing Nginx server \ e[0m"
 yum install nginx -y
-echo -e "\e[31m remove default content \ e[0m"
+echo -e "\e[36m remove default content \ e[0m"
 rm -rf /usr/share/nginx/html/*
-echo -e "\e[31m Download frontend content \ e[0m"
+echo -e "\e[36m Download frontend content \ e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-echo -e "\e[31m Extract frontend content \ e[0m"
+echo -e "\e[36m Extract frontend content \ e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 #configuration file
-echo -e "\e[31m start nginx server \ e[0m"
+echo -e "\e[36m start nginx server \ e[0m"
 systemctl enable nginx
 systemctl restart nginx
