0436d8a4e41d9dd49ae42057a703ae86b6449c09
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-01 16:28:25 +0530

    Start mongodb

--- 2.mongodb.sh
+++ 2.mongodb.sh
@@ -1,7 +1,7 @@
 echo -e "\e[31m Copying mongodb \e[0m"
 cp mongo.repo /etc/yum.repos.d/mongodb.repo
 echo -e "\e[34m Installing mongodb \e[0m"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>/tmp/roboshop.log
 echo -e "\e[33m start mongodb \e[0m"
-systemctl enable mongod
-systemctl restart mongod
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log
