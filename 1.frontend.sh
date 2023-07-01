150527650fb4bf8fbc0f82ad5299cacf8c87c101
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-01 15:12:57 +0530

    start nginx server with readble steps,, removing o/p content in to logs using error redirector

--- 1.frontend.sh
+++ 1.frontend.sh
@@ -3,9 +3,9 @@ yum install nginx -y >/tmp/roboshop.log
 echo -e "\e[35m remove default content \e[0m"
 rm -rf /usr/share/nginx/html/*
 echo -e "\e[34m Download frontend content \e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 2>/tmp/error.log
 echo -e "\e[33m Extract frontend content \e[0m"
-cd /usr/share/nginx/html >/tmp/roboshop.log
+cd /usr/share/nginx/html
 unzip /tmp/frontend.zip >/tmp/roboshop.log
 #configuration file
 echo -e "\e[32m start nginx server \e[0m"
