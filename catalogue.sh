aacbaa143cd091d82d7a9412c11803ae896be148
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-05 11:34:03 +0530

    start catalogue service

--- catalogue.sh
+++ catalogue.sh
@@ -23,7 +23,7 @@ cd /app
 npm install &>>/tmp/roboshop.log
 
 echo -e "\e[36m setup systemd service \e[0m"
-cp catalogue.service/etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+cp /root/Roboshop-shell/catalogue.service/etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
 
 echo -e "\e[36m  start catalogue service \e[0m"
 
