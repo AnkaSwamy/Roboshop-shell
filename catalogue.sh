07f23006e9aded5d04d960d4b9277b5ab055064a
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-05 15:18:39 +0530

    frontend

--- catalogue.sh
+++ catalogue.sh
@@ -1,40 +1,40 @@
 echo -e "\e[31m configure nodejs repos \e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>/tmp/roboshop.log
 
 echo -e "\e[31m  Install nodejs \e[0m"
-yum install nodejs -y &>>/tmp/roboshop.log
+yum install nodejs -y  &>>/tmp/roboshop.log
 
 echo -e "\e[31m add application user \e[0m"
-useradd roboshop &>>/tmp/roboshop.log
+useradd roboshop  &>>/tmp/roboshop.log
 
 echo -e "\e[31m Create application directory \e[0m"
 rm -rf /app
 mkdir /app
 
 echo -e "\e[31m  download application content \e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip  &>>/tmp/roboshop.log
 cd /app
 
 echo -e "\e[31m extract Application content \e[0m"
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+unzip /tmp/catalogue.zip  &>>/tmp/roboshop.log
 cd /app
 
 echo -e "\e[31m Install nodejs dependencies \e[0m"
-npm install &>>/tmp/roboshop.log
+npm install  &>>/tmp/roboshop.log
 
 echo -e "\e[31m setup systemd service \e[0m"
-cp /root/Roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+cp /root/Roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service  &>>/tmp/roboshop.log
 
 echo -e "\e[31m  start catalogue service \e[0m"
 systemctl daemon-reload
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl restart catalogue &>>/tmp/roboshop.log
+systemctl enable catalogue  &>>/tmp/roboshop.log
+systemctl restart catalogue  &>>/tmp/roboshop.log
 
 echo -e "\e[31m  copy mongodb repo file \e[0m"
-cp /root/Roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
+cp /root/Roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo  &>>/tmp/roboshop.log
 
 echo -e "\e[31m  Install Mongodb client \e[0m"
-yum install mongodb-org-shell -y &>>/tmp/roboshop.log
+yum install mongodb-org-shell -y  &>>/tmp/roboshop.log
 
 echo -e "\e[31m Load schema \e[0m"
-mongo --host mongodb-dev.ankadevopsb73.store </app/schema/catalogue.js &>>/tmp/roboshop.log
+mongo --host mongodb-dev.ankadevopsb73.store </app/schema/catalogue.js  &>>/tmp/roboshop.log
