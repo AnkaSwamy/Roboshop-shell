5defce1c75e14b2fd18382db045bba0a083d5313
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-05 14:05:33 +0530

    redis server

--- Redis.sh
+++ Redis.sh
@@ -10,6 +10,6 @@ yum install redis -y &>>/tmp/roboshop.log
 echo -e "\e[31m update redis listen address \e[0m"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>/tmp/roboshop.log
 
-echo -e "\e[31m  start redis service \e[0m"
+echo -e "\e[31m  start the redis service \e[0m"
 systemctl enable redis &>>/tmp/roboshop.log
 systemctl restart redis  &>>/tmp/roboshop.log
