36338ac184508f7b33aa2e399547c279ed31e6cc
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-05 15:08:09 +0530

    frontend

--- 2.mongodb.sh
+++ 2.mongodb.sh
@@ -7,6 +7,6 @@ yum install mongodb-org -y &>>/tmp/roboshop.
 echo -e "\e[31m Update mongodb listen address \e[0m"
 sed -i  's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
-echo -e "\e[31m start mongodb \e[0m"
+echo -e "\e[31m start mongodb server \e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
 systemctl restart mongod &>>/tmp/roboshop.log
