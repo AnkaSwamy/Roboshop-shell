450f946b638b22c67d92f42b3f607223fa3265c4
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-06 18:28:10 +0530

    Mongodb server setup

--- mongodb.sh
+++ mongodb.sh
@@ -1,12 +1,12 @@
-echo -e "\e[31m Copying mongodb repo \e[0m"
-cp /root/Roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo
+echo -e "\e[31m Copy mongodb repo file \e[0m"
+cp /root/Roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo  &>>/tmp/roboshop.log
 
 echo -e "\e[31m Installing mongodb server \e[0m"
-yum install mongodb-org -y &>>/tmp/roboshop.
+yum install mongodb-org -y  &>>/tmp/roboshop.log
 
 echo -e "\e[31m Update mongodb listen address \e[0m"
 sed -i  's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
-echo -e "\e[31m start mongodb server \e[0m"
-systemctl enable mongod &>>/tmp/roboshop.log
-systemctl restart mongod &>>/tmp/roboshop.log
+echo -e "\e[31m Start mongodb server \e[0m"
+systemctl enable mongod  &>>/tmp/roboshop.log
+systemctl restart mongod  &>>/tmp/roboshop.log
