7dcd614a59128111cc5e6a95bb0c986220f36c05
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-06 18:24:41 +0530

    Frontend service

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,7 @@
 echo -e "\e[31m Installing Nginx server \e[0m"
 yum install nginx -y &>>/tmp/roboshop.log
 
-echo -e "\e[31m remove default content \e[0m"
+echo -e "\e[31m Remove default content \e[0m"
 rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
 echo -e "\e[31m Download frontend content \e[0m"
@@ -11,9 +11,9 @@ echo -e "\e[31m Extract frontend content \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
-echo -e "\e[31m update the configuration \e[0m"
+echo -e "\e[31m Update the configuration \e[0m"
 cp /root/Roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
 
-echo -e "\e[31m start nginx server \e[0m"
+echo -e "\e[31m Start nginx server \e[0m"
 systemctl enable nginx &>>/tmp/roboshop.log
 systemctl restart nginx &>>/tmp/roboshop.log
