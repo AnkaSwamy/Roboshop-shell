01e5dfefbf32da33a4c3571263ab5f3a9ca14500
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-10 15:18:13 +0530

    variables concept in components,,keeps code dry

--- catalogue.sh
+++ catalogue.sh
@@ -1,41 +1,47 @@
-echo -e "\e[31m Configure nodejs repos \e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash   &>>/tmp/roboshop.log
+component= component
+color="\e[31m"
+nocolor="\e[0m"
+log_file="&>>/tmp/roboshop.log"
+app_path="/app"
 
-echo -e "\e[31m Install nodejs \e[0m"
-yum install nodejs -y  &>>/tmp/roboshop.log
+echo -e " ${color} Configure nodejs repos ${nocolor}"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash   ${logfile}
 
-echo -e "\e[31m Add application user \e[0m"
-useradd roboshop  &>>/tmp/roboshop.log
+echo -e "${color} Install nodejs ${nocolor}"
+yum install nodejs -y  ${logfile}
 
-echo -e "\e[31m Create application directory \e[0m"
-rm -rf /app
-mkdir /app
+echo -e "${color} Add application user ${nocolor}"
+useradd roboshop  ${logfile}
 
-echo -e "\e[31m Download application content \e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip   &>>/tmp/roboshop.log
-cd /app
+echo -e "${color} Create application directory ${nocolor}"
+rm -rf app_path
+mkdir app_path
 
-echo -e "\e[31m Extract application content \e[0m"
-unzip /tmp/catalogue.zip  &>>/tmp/roboshop.log
-cd /app
+echo -e "${color} Download application content ${nocolor}"
+curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip   ${logfile}
+cd app_path
 
-echo -e "\e[31m Install nodejs dependencies \e[0m"
-npm install  &>>/tmp/roboshop.log
+echo -e "${color} Extract application content ${nocolor}"
+unzip /tmp/$component.zip  ${logfile}
+cd app_path
 
-echo -e "\e[31m Setup systemd service \e[0m"
-cp /root/Roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service  &>>/tmp/roboshop.log
+echo -e "${color} Install nodejs dependencies ${nocolor}"
+npm install  ${logfile}
 
-echo -e "\e[31m  Start the catalogue service \e[0m"
-systemctl daemon-reload   &>>/tmp/roboshop.log
-systemctl enable catalogue  &>>/tmp/roboshop.log
-systemctl restart catalogue  &>>/tmp/roboshop.log
+echo -e "${color} Setup systemd service ${nocolor}"
+cp /root/Roboshop-shell/$component.service /etc/systemd/system/$component.service  ${logfile}
 
-echo -e "\e[31m Copy mongodb repo file \e[0m"
-cp /root/Roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo  &>>/tmp/roboshop.log
+echo -e "${color}  Start the $component service ${nocolor}"
+systemctl daemon-reload   ${logfile}
+systemctl enable $component  ${logfile}
+systemctl restart $component  ${logfile}
 
-echo -e "\e[31m Install Mongodb client \e[0m"
-yum install mongodb-org-shell -y  &>>/tmp/roboshop.log
+echo -e "${color} Copy mongodb repo file ${nocolor}"
+cp /root/Roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo  ${logfile}
 
-echo -e "\e[31m Load Schema \e[0m"
-mongo --host mongodb-dev.ankadevopsb73.store </app/schema/catalogue.js   &>>/tmp/roboshop.log
+echo -e "${color} Install Mongodb client ${nocolor}"
+yum install mongodb-org-shell -y  ${logfile}
+
+echo -e "${color} Load Schema ${nocolor}"
+mongo --host mongodb-dev.ankadevopsb73.store <app_path/schema/$component.js   ${logfile}
 
