ddf85a68f1c1f37b19653e69b36c155726e28138
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-10 15:41:09 +0530

    variables concept in components,,keeps code dry

--- catalogue.sh
+++ catalogue.sh
@@ -1,10 +1,10 @@
-component=component
+catalogue=component
 color="\e[31m"
 nocolor="\e[0m"
-log_file="/tmp/roboshop.log"
 app_path="/app"
+log_file="/tmp/roboshop.log"
 
-echo -e " ${color} Configure nodejs repos ${nocolor}"
+echo -e "${color} Configure nodejs repos ${nocolor}"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash   &>>$log_file
 
 echo -e "${color} Install nodejs ${nocolor}"
@@ -15,7 +15,7 @@ useradd roboshop  &>>$log_file
 
 echo -e "${color} Create application directory ${nocolor}"
 rm -rf
-mkdir ${app_path} &>>$log_file
+mkdir ${app_path}  &>>$log_file
 
 echo -e "${color} Download application content ${nocolor}"
 curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip   &>>$log_file
@@ -44,5 +44,3 @@ yum install mongodb-org-shell -y  &>>$log_file
 
 echo -e "${color} Load Schema ${nocolor}"
 mongo --host mongodb-dev.ankadevopsb73.store <${app_path}/schema/$component.js   &>>$log_file
-
-
