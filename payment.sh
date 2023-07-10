1e69789661f0667d67e80e8e50e7f48b83dacbbf
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-10 13:10:10 +0530

    payment service

--- payment.sh
+++ payment.sh
@@ -1,10 +1,10 @@
 echo -e "\e[31m Install python \e[0m"
 yum install python36 gcc python3-devel -y  &>>/tmp/roboshop.log
 
-echo -e "\e[31m add application user \e[0m"
+echo -e "\e[31m Add application user \e[0m"
 useradd roboshop   &>>/tmp/roboshop.log
 
-echo -e "\e[31m create application directory \e[0m"
+echo -e "\e[31m Create application directory \e[0m"
 rm -rf /app   &>>/tmp/roboshop.log
 mkdir /app
 
@@ -19,11 +19,11 @@ echo -e "\e[31m Install application dependencies \e[0m"
 cd /app
 pip3.6 install -r requirements.txt  &>>/tmp/roboshop.log
 
-echo -e "\e[31m copy setup systemd service \e[0m"
+echo -e "\e[31m  Setup systemd service \e[0m"
 cp /root/Roboshop-shell/payment.service /etc/systemd/system/payment.service  &>>/tmp/roboshop.log
 
 
-echo -e "\e[31m start payment service \e[0m"
+echo -e "\e[31m Start payment service \e[0m"
 systemctl daemon-reload   &>>/tmp/roboshop.log
 systemctl enable payment   &>>/tmp/roboshop.log
 systemctl start payment    &>>/tmp/roboshop.log
