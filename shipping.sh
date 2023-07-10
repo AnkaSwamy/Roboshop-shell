e831441da2a5b45029be8b49077f4cc6363bca36
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-10 13:05:44 +0530

    shipping service

--- shipping.sh
+++ shipping.sh
@@ -9,7 +9,7 @@ rm -rf /app  &>>/tmp/roboshop.log
 mkdir /app
 
 echo -e "\e[31m Download application content \e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip  &>>/tmp/roboshop.log
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip   &>>/tmp/roboshop.log
 cd /app
 
 echo -e "\e[31m Extract application content \e[0m"
@@ -18,13 +18,13 @@ cd /app
 
 echo -e "\e[31m  Download maven dependencies \e[0m"
 mvn clean package  &>>/tmp/roboshop.log
-mv target/shipping-1.0.jar shipping.jar  &>>/tmp/roboshop.log
+mv target/shipping-1.0.jar shipping.jar   &>>/tmp/roboshop.log
 
 echo -e "\e[31m Install mysql client \e[0m"
 yum install mysql -y  &>>/tmp/roboshop.log
 
 echo -e "\e[31m load schema \e[0m"
-mysql -h mysql-dev.ankadevopsb73.store -uroot -pRoboShop@1 < /app/schema/shipping.sql  &>>/tmp/roboshop.log
+mysql -h  mysql-dev.ankadevopsb73.store -uroot -pRoboShop@1 < /app/schema/shipping.sql   &>>/tmp/roboshop.log
 
 echo -e "\e[31m setup systemd service \e[0m"
 cp /root/Roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>>/tmp/roboshop.log
