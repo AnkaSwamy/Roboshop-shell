16dc87a88adbc80245fb84be61d30c68dd6d4caa
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-12 11:18:32 +0530

    special varibles

--- vars.sh
+++ vars.sh
@@ -1,5 +1,12 @@
 #special variables
-echo script name - $0
-echo first argument  - $1
-echo all arguments - $*
-echo no of arguments  - $#
+#echo script name - $0
+#echo first argument  - $1
+#echo all arguments - $*
+#echo no of arguments  - $#
+
+print_arguments() {
+  echo first argument $1
+  echo second argument $1
+  echo all  argument $*
+  echo no of arguments $#
+  }
