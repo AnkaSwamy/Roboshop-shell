f631a8a110e0cc5df82fb81789ecd66219388df3
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-12 09:55:28 +0530

    using commmon file

--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,5 @@
 source common.sh
+component=frontend
 
 echo -e "${color} Installing Nginx server ${nocolor} "
 yum install nginx -y &>>$log_file
@@ -6,14 +7,14 @@ yum install nginx -y &>>$log_file
 echo -e "${color} Remove default content ${nocolor}"
 rm -rf /usr/share/nginx/html/* &>>$log_file
 
-echo -e "${color} Download frontend content ${nocolor}""
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+echo -e "${color} Download frontend content ${nocolor}"
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip  &>>$log_file
 
 echo -e "${color} Extract frontend content ${nocolor}"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>>$log_file
+unzip /tmp/${component}.zip &>>$log_file
 
-echo -e "${color} Update the configuration ${nocolor}""
+echo -e "${color} Update the configuration ${nocolor}"
 cp /root/Roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf  &>>$log_file
 
 echo -e "${color} Start nginx server ${nocolor}"
